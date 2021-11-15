ff711eae88fdf3abef865a4d208c7e8d6c22d00c
0s4k4 <0s4k4@DESKTOP-NDBHG8U>
2021-11-14 22:27:46 -0600

    Agregar archivos de proyecto.

---
+++ GestionDeLibreria/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace GestionDeLibreria.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string SerialNumber { get; set; }
+
+        public string Author { get; set; }
+
+        public string Publisher { get; set; }
+
+    }
+}
